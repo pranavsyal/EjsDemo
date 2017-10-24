6aa69a6c1fbe3fe7b00e0e511e3d7ad8e1c1c97a
pranav.syal7 <pranav.syal7@gmail.com>
2017-10-24 04:25:30 -0400

    add Ejs

---
+++ app.js
@@ -0,0 +1,10 @@
+var express=require("express");
+var app=express();
+
+app.get("/", function(req, res){
+  res.render("home.ejs");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+  console.log("Server is Listening!!!");
+});
